b59668b80b1ef43c5f71b88c860c3be61053c3f0
tongtongyin <37565148+tongtongyin@users.noreply.github.com>
2018-08-20 21:19:54 +0800

    Add files via upload

--- lv_02_进程池.py
+++ lv_02_进程池.py
@@ -3,7 +3,7 @@ import os,time
 
 def worker(name):
     """定义子进程的执行函数"""
-    print("this is child process pid = %d" % os.getpid())
+    print("this is child process pid = %d" % (os.getpid()))
     start = time.time()
     time.sleep(2)
     end = time.time()
@@ -15,10 +15,13 @@ if __name__ == "__main__":
     p = Pool(3)
     # 每三个进程同时执行
     for x in range(10):
-        # 创建进程对象
-        p.apply_async(worker,args=(x,))
+        # 创建进程对象，异步
+        p.apply_async(worker,args=("text",))
+        # p.apply(worker,args=(x,))     同步
     # 关闭进程池
     p.close()
     p.join()
 
     print("end")
+
+"""异步：首先申请三个进程，2秒之后，再把这三个进程同时进行 在把这三个进程同时进行"""
